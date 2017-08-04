4a1292af502a5180e14729d73379d89d6a81627b
游品尧 <497400448@qq.com>
2017-08-04 23:56:36 +0800

    优化

--- routes/get_realtime_news_ws.js
+++ routes/get_realtime_news_ws.js
@@ -6,143 +6,84 @@ const WebSocket = require('websocket').client;
 
 module.exports = function (app) {
   const headers = config.headers();
-  const ws9503 = new WebSocket();
-  let ws9503Conection = null;
-  const ws9507 = new WebSocket();
-  let ws9507Conection = null;
 
   headers.Origin = 'http://www.yuncaijing.com';
 
-  const ws9503Arr = {};
-  const ws9507Arr = {};
-
-  connectWs9503();
-  connectWs9507();
-
   app.ws('/ws9503', function (ws, req) {
-    const uuid = uuidv4();
-    ws9503Arr[uuid] = ws;
-
-    ws.on('open', function (msg) {
-      console.log(chalk.green('open ws9503: '), msg);
-    });
-    ws.on('error', function (msg) {
-      console.log(chalk.green('error ws9503: '), msg);
-    });
-    ws.on('message', function (msg) {
-      console.log(chalk.green('receive message ws9503: '), msg);
-      if (ws9503Conection) {
-        console.log(chalk.green('send message ws9503: '), msg);
-        if (ws9503Conection.readyState === ws9503Conection.OPEN) {
-          ws9503Conection.sendUTF(msg);
-        }
-      }
-      console.log();
-    });
-    ws.on('close', function (msg) {
-      console.log('close', 'ws9503');
-      delete ws9503Arr[uuid];
-    });
+    doConnect('ws9503', ws);
   });
-
-
   app.ws('/ws9507', function (ws, req) {
-    const uuid = uuidv4();
-    ws9507Arr[uuid] = ws;
+    doConnect('ws9507', ws);
+  });
+
+  function doConnect(type, ws) {
+    let connection = null;
+
+    connectRemoveWs(type, function (c) {
+      connection = c;
+    }, function (data) {
+      if (ws.readyState === ws.OPEN) {
+        ws.send(data);
+      } else {
+        ws.close();
+      }
+    });
 
     ws.on('open', function (msg) {
-      console.log(chalk.green('open ws9507: '), msg);
+      console.log(chalk.green(`open ${type}: `), msg);
     });
     ws.on('error', function (msg) {
-      console.log(chalk.green('error ws9507: '), msg);
+      console.log(chalk.green(`error ${type}: `), msg);
+      connection.close();
     });
     ws.on('message', function (msg) {
-      console.log(chalk.green('receive message ws9507: '), msg);
-      if (ws9507Conection) {
-        console.log(chalk.green('send message ws9507: '), msg);
-        if (ws9507Conection.readyState === ws9507Conection.OPEN) {
-          ws9507Conection.sendUTF(msg);
+      console.log(chalk.green(`receive message ${type}: `), msg);
+      doSend();
+
+      function doSend() {
+        if (connection) {
+          console.log(chalk.green(`send message ${type}: `), msg);
+          if (connection.readyState === connection.OPEN) {
+            connection.sendUTF(msg);
+          }
+        } else {
+          setTimeout(doSend, 300);
         }
       }
+
       console.log();
     });
     ws.on('close', function (msg) {
-      console.log('close', 'ws9507');
-      delete ws9507Arr[uuid];
-    });
-  });
-
-
-  function connectWs9503() {
-    ws9503.connect(config.urls.ws9503, undefined, headers.Origin, headers);
-    ws9503.on('connectFailed', function (error) {
-      console.log('Connect Failed ws9503: ' + error.toString());
-    });
-
-    ws9503.on('connect', function (connection) {
-      ws9503Conection = connection;
-
-      console.log(chalk.green('WebSocket Client Connected ws9503'));
-
-      connection.on('error', function (error) {
-        connectWs9503.close();
-        console.log(chalk.red('Connection Error ws9503: '), error.toString());
-      });
-      connection.on('close', function (error) {
-        console.log(chalk.red('echo-protocol Connection Closed ws9503: '), error.toString());
-
-        setTimeout(connectWs9503, 1000);
-      });
-      connection.on('message', function (event) {
-        console.log(chalk.green('message ws9503 from remote'));
-        console.log();
-
-        for (const ws in ws9503Arr) {
-          if (ws9503Arr[ws].readyState === ws9503Arr[ws].OPEN) {
-            ws9503Arr[ws].send(event.utf8Data);
-          } else {
-            ws9503Arr[ws].close();
-            delete ws9503Arr[ws];
-          }
-        }
-      });
+      console.log('close', type);
+      connection.close();
     });
   }
 
-  function connectWs9507() {
-    ws9507.connect(config.urls.ws9507, undefined, headers.Origin, headers);
-    ws9507.on('connectFailed', function (error) {
-      console.log('Connect Failed ws9507: ' + error.toString());
+  function connectRemoveWs(type, callback, message) {
+    const ws = new WebSocket();
+    ws.connect(config.urls[type], undefined, headers.Origin, headers);
+    ws.on('connectFailed', function (error) {
+      console.log(`remote Connect Failed ${type}: ` + error.toString());
     });
 
-    ws9507.on('connect', function (connection) {
-      ws9507Conection = connection;
+    ws.on('connect', function (connection) {
+      callback(connection);
 
-      console.log(chalk.green('WebSocket Client Connected ws9507'));
+      console.log(chalk.green(`remote WebSocket Client Connected ${type}`));
 
       connection.on('error', function (error) {
-        connectWs9507.close();
-        console.log(chalk.red('Connection Error ws9507: '), error.toString());
+        console.log(chalk.red(`remote Connection Error ${type}: `), error.toString());
+        connect(type, callback, message);
       });
       connection.on('close', function (error) {
-        console.log(chalk.red('echo-protocol Connection Closed ws9507: '), error.toString());
-
-        setTimeout(connectWs9507, 1000);
+        console.log(chalk.red(`remote echo-protocol Connection Closed ${type}: `), error.toString());
       });
       connection.on('message', function (event) {
-        console.log(chalk.green('message ws9507 from remote'));
         console.log();
-
-        for (const ws in ws9507Arr) {
-          if (ws9507Arr[ws].readyState === ws9507Arr[ws].OPEN) {
-            ws9507Arr[ws].send(event.utf8Data);
-          } else {
-            ws9507Arr[ws].close();
-            delete ws9507Arr[ws];
-          }
-        }
+        console.log(chalk.green(`remote message ${type} from`));
+        console.log();
+        message(event.utf8Data);
       });
     });
   }
-
 }
